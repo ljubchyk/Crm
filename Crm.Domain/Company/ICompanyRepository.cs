7d0a756f053ef8e870a2037935fc0ce8d64a1419
Liubomyr Popadiuk <ljubchyk@outlook.com>
2022-05-24 21:27:41 +0300

    ...

---
+++ Crm.Domain/Company/ICompanyRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace Crm.Domain.Company;
+
+public interface ICompanyRepository
+{
+    Task Create(Company company);
+    Task Update(Company company);
+    Task<Company> Get(Guid id);
+    Task<List<Company>> GetList();
+}
--- Crm/Controllers/CompaniesController.cs
+++ Crm/Controllers/CompaniesController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Crm.Domain.Company;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Crm.Controllers
 {
@@ -6,14 +7,42 @@ namespace Crm.Controllers
     [Route("[controller]")]
     public class CompaniesController : ControllerBase
     {
-        public CompaniesController()
+        private readonly ICompanyRepository companyRepository;
+
+        public CompaniesController(ICompanyRepository companyRepository)
         {
+            this.companyRepository = companyRepository;
         }
 
-        //[HttpGet]
-        //public IActionResult Get()
-        //{
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(Guid id)
+        {
+            var company = await companyRepository.Get(id);
+            if (company is null)
+            {
+                return NotFound();
+            }
 
-        //}
+            return Ok(company);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetList()
+        {
+            var companies = await companyRepository.GetList();
+            return Ok(companies);
+        }
+
+        [HttpGet("{id}/owners")]
+        public async Task<IActionResult> GetOwners(Guid id)
+        {
+            var company = await companyRepository.Get(id);
+            if (company is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(company.Owners);
+        }
     }
 }
