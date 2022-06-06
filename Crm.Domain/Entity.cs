a6ffeb669e24c142757b9112a8c47cf9d0069913
ljubchyk <ljubchyk@gmail.com>
2022-06-06 21:57:45 +0300

    1-add-domainevents

--- Crm.Domain/Entity.cs
+++ Crm.Domain/Entity.cs
@@ -3,21 +3,27 @@
 public abstract class Entity : IEquatable<Entity>
 {
     private readonly Guid[] id;
+    private readonly List<DomainEvent> domainEvents;
 
     private Entity()
     {
-
+        domainEvents = new List<DomainEvent>();
     }
 
     protected Entity(Guid id)
+        : this()
     {
         this.id = new[] { id };
     }
 
     protected Entity(Guid id1, Guid id2)
+        : this()
     {
         id = new[] { id1, id2 };
     }
+
+    public IReadOnlyList<DomainEvent> DomainEvents => 
+        domainEvents;
 
     public bool Equals(Entity other)
     {
@@ -37,5 +43,10 @@ public abstract class Entity : IEquatable<Entity>
     public override bool Equals(object obj)
     {
         return Equals(obj as Entity);
+    }
+
+    protected void AddDomainEvent(DomainEvent domainEvent)
+    {
+        domainEvents.Add(domainEvent);
     }
 }
